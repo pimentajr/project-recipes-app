026234e9214fde53ea1ebd0f15b15729de1b58aa
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-08-13 18:11:58 -0300

    Testando visual 01

--- src/pages/Explorer.js
+++ src/pages/Explorer.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ScrollContainer from 'react-indiana-drag-scroll';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ExploreFood from '../images/exploreFood.png';
+import ExploreDrink from '../images/exploreDrink.png';
+import './css/ExplorePage.css';
+
+import {
+  Title,
+  Yellow,
+  Green,
+  Red,
+  CardContainer,
+  ImageFood,
+  ImageShadow,
+  ImageDrink,
+  CardTitle,
+  CardTitleBordered,
+} from './styles/ExplorerStyle';
 
 function Explorer() {
   const headerProps = {
@@ -11,25 +28,38 @@ function Explorer() {
   };
 
   return (
-    <div>
+    <div className="explore-page">
       <Header props={ headerProps } />
-      <h1>Explorar...</h1>
-      <Link to="/explorar/comidas">
-        <button
-          type="button"
-          data-testid="explore-food"
-        >
-          Explorar Comidas
-        </button>
-      </Link>
-      <Link to="/explorar/bebidas">
-        <button
-          type="button"
-          data-testid="explore-drinks"
-        >
-          Explorar Bebidas
-        </button>
-      </Link>
+      <Title>
+        Explorar
+        <Yellow>.</Yellow>
+        <Green>.</Green>
+        <Red>.</Red>
+      </Title>
+      <ScrollContainer className="scroll-container">
+        <CardContainer food>
+          <Link className="explore-link" to="/explorar/comidas">
+
+            <CardTitle food>FOOD</CardTitle>
+            <CardTitleBordered food>FOOD</CardTitleBordered>
+
+            <ImageFood src={ ExploreFood } />
+            <ImageShadow />
+
+          </Link>
+        </CardContainer>
+
+        <CardContainer drink>
+          <Link className="explore-link" to="/explorar/bebidas">
+
+            <CardTitle drink>DRINK</CardTitle>
+            <CardTitleBordered drink>DRINK</CardTitleBordered>
+
+            <ImageDrink src={ ExploreDrink } />
+
+          </Link>
+        </CardContainer>
+      </ScrollContainer>
       <Footer />
     </div>
   );
--- src/pages/styles/ExplorerStyle.js
+++ src/pages/styles/ExplorerStyle.js
@@ -1,5 +1,100 @@
 import styled from 'styled-components';
 
-const Title = styled.h1`
+export const Title = styled.h1`
   font-family: 'Poppins', sans-serif;
+  font-size: 40px;
+  line-height: 72px;
+  letter-spacing: 0.1em;
+  text-align: center;
+  margin-top: 40px;
+  position: relative;
+  left: 10px;
+  `;
+
+export const Yellow = styled.span`
+  font-size: 65px;
+  color: #FFC729;
+`;
+
+export const Green = styled.span`
+  font-size: 65px;
+  color: #A0D468;
+`;
+
+export const Red = styled.span`
+  font-size: 65px;
+  color: #FC6E51;
+`;
+
+export const CardContainer = styled.div`
+  min-width: ${(props) => (props.food ? '230px' : '250px')};
+  height: 380px;
+  background: rgba(200, 200, 200, 0.8);
+  border-radius: 25px;
+  margin: 0 40px 30px 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ImageFood = styled.img`
+  height: 250px;
+  align-self: center;
+  position: relative;
+  left: 5px;
+  z-index: 98;
+  text-shadow: 2px 2px 2px black;
+`;
+
+export const ImageDrink = styled.img`
+  height: 340px;
+  align-self: center;
+  position: relative;
+  bottom: 50px;
+  left: 15px;
+  z-index: 98;
+  text-shadow: 2px 2px 2px black;
+`;
+
+export const ImageShadow = styled.div`
+  width: 180px;
+  height: 20px;
+  border-radius: 40px;
+  box-shadow: 0 25px 25px rgba(0,0,0,0.90);
+  position: relative;
+  top: -40px;
+  left: 50px;
+  z-index: 97;
+`;
+
+export const CardTitle = styled.h2`
+  font-family: 'Poppins', sans-serif;
+  font-size: 72px;
+  line-height: 72px;
+  letter-spacing: 0.1em;
+  margin: ${(props) => (props.food ? '40px 0 0 30px' : '40px 0 0 12px')};
+  color: rgba(44, 44, 44, 08);
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  ${CardContainer}:hover & {
+    transition: 0.1s;
+    transform: scale(1.2, 1.2) translate(3px);
+  }
+`;
+
+export const CardTitleBordered = styled.h2`
+  font-family: 'Poppins', sans-serif;
+  font-size: 72px;
+  line-height: 72px;
+  letter-spacing: 0.1em;
+  color: transparent;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: rgba(44, 44, 44, 08);
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  margin: ${(props) => (props.food ? '-72px 0 -35px 30px' : '-72px 0 -35px 12px')};
+  z-index: 99;
+  position: relative;
+  ${CardContainer}:hover & {
+    transition: 0.1s;
+    transform: scale(1.2, 1.2) translate(3px);
+  }
 `;
