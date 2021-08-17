a37ef2277094b79c3c392220a5441a0b1af49187
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-08-17 19:47:45 -0300

    Pagina explorar concluida

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 
 export default function Explore() {
+  const history = useHistory();
+
   return (
     <div>
       <Header pageTitle="Explorar" itHasNotSearchButton />
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => history.push('explorar/comidas') }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('explorar/bebidas') }
+      >
+        Explorar Bebidas
+      </button>
       <Footer />
     </div>
   );
