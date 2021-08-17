271c1268dce3adbd885cb672b33744b852800f28
OldRoge <rogeriomrjr@icloud.com>
2021-08-16 22:56:29 -0300

    Fixing lint errors

--- src/components/common/HeaderDetails/HeaderDetails.jsx
+++ src/components/common/HeaderDetails/HeaderDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import useFavoriteRecipies from '../../../hooks/useFavoriteRecipies';
 import shareIcon from '../../../images/shareIcon.svg';
 import whiteHeartIcon from '../../../images/whiteHeartIcon.svg';
