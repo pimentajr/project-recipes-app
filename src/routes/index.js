7995f976063e5e5228970d4c21ef772c3ed7cfe2
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-08-17 23:56:42 -0300

    Altera definicao da rota para a pagina NotFound

--- src/routes/index.js
+++ src/routes/index.js
@@ -38,7 +38,7 @@ class Routes extends React.Component {
           component={ ExploreIngredients }
         />
         <Route exact path="/explorar/comidas/area" component={ ExploreLocation } />
-        <Route exact path="/explorar/bebidas/area" component={ NotFound } />
+        <Route exact path="*" component={ NotFound } />
         <Route exact path="/perfil" component={ Profile } />
         <Route exact path="/receitas-feitas" component={ RecipesDone } />
         <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
